d8b72c9d44af34383a6ec7be98100482b3243563
nganga <huyennga154@gmail.com>
2020-10-15 14:10:45 +0900

    Solve the following problems: + 938. Range Sum of BST + 339. Nested List Weight Sum + 13. Roman to Integer

--- algorithms/1.Range Sum of BST/rangeSumBST.py
+++ algorithms/1.Range Sum of BST/rangeSumBST.py
@@ -1,4 +1,30 @@
 # 938. Range Sum of BST
 # https://leetcode.com/problems/range-sum-of-bst/
 
+def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+    res = 0
+    if root is None:
+        return 0
+    if L <= root.val <= R:
+        res += root.val
+    if root.left:
+        res += self.rangeSumBST(root.left, L, R)
+    if root.right:
+        res += self.rangeSumBST(root.right, L, R)
+    return res
 
+# improved algorithm - referred to Leetcode's solution. 
+def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+    res = 0
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        if node:
+            if L <= node.val <= R:
+                res += node.val
+            if L < node.val:
+                stack.append(node.left)
+            if node.val < R:
+                stack.append(node.right)
+    return res
+                
---
+++ algorithms/1.Roman to Integer/roman2Int.py
@@ -0,0 +1,54 @@
+# 13. Roman to Integer
+# https://leetcode.com/problems/roman-to-integer/
+  
+def romanToInt(str): 
+    value = { 
+        'M': 1000, 
+        'D': 500, 
+        'C': 100, 
+        'L': 50, 
+        'X': 10, 
+        'V': 5, 
+        'I': 1
+    }
+    res = 0
+    i = 0
+    
+    while i < len(str):
+        s1 = value[str[i]] 
+        if i < len(str) - 1:
+            s2 = value[str[i + 1]]
+            if s1 >= s2:
+                res += s1
+                i += 1
+            else:
+                res = res - s1 + s2
+                i += 2
+        else:
+            res += s1
+            i += 1
+    return res 
+
+# Improved algorithm - referred to Leetcode's Solution
+def romanToInt_update(s): 
+    value = { 
+        'M': 1000, 
+        'D': 500, 
+        'C': 100, 
+        'L': 50, 
+        'X': 10, 
+        'V': 5, 
+        'I': 1
+    }
+    res = value[s[-1]]
+    for i in reversed(range(len(s) - 1)):
+        if value[s[i]] < value[s[i+1]]:
+            res -= value[s[i]]
+        else:
+            res += value[s[i]]
+    return res
+
+
+if __name__ == "__main__":
+    print(romanToInt("MCMXCIV"))
+    print(romanToInt_update("MCMXCIV"))
