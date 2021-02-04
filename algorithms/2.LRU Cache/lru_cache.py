7957048f8a41acddea834afa9ff7c0fecef20740
nganga <huyennga154@gmail.com>
2021-02-04 16:14:28 +0900

    Add template of LRU cache problem

---
+++ algorithms/2.LRU Cache/lru_cache.py
@@ -0,0 +1,19 @@
+# 146. LRU Cache
+# https://leetcode.com/problems/lru-cache/
+
+# Least Recently Used cache
+# for fast look ups, we will use hash maps
+# for fast removals, we will use double ended queue
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        
+    def get(self, key: int) -> int:
+        
+    def put(self, key: int, value: int) -> None:
+        
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
