e365b40deb74155a1d882b3cc75b258f556f5bdc
nganga <huyennga154@gmail.com>
2022-01-02 17:53:51 +0900

    Add solution (using sliding window) to resolve Longest Substring without repeating chars

--- algorithms/2.Longest Substring Without Repeating Chars/lenOfLongestSubStr.py
+++ algorithms/2.Longest Substring Without Repeating Chars/lenOfLongestSubStr.py
@@ -24,6 +24,21 @@ def lengthOfLongestSubstring(s: str) -> int:
         res[c] = i
     return max_len
    
+# using sliding widown
+def lengthOfLongestSubstring(s: str) -> int:
+    m = {}
+    left, right = 0, 0
+    ans = 0
+    n = len(s)
+    while(left < n and right < n):
+        el = s[right]
+        if el in m:
+            left = max(left, m[el] + 1)
+        m[el] = right
+        ans = max(ans, right - left + 1)
+        right += 1
+    return ans
+
 if __name__ == "__main__":
     print(lengthOfLongestSubstring("abcabcbb"))
     print(lengthOfLongestSubstring("pwwkew"))
